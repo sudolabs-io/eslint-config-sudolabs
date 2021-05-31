3868493acac50e30630d897df4efe4ac462059d8
Michal Vanko <mklakakilli@gmail.com>
2021-05-31 10:50:56 +0200

    Turn off react prop-types for typescript-react

--- react.js
+++ react.js
@@ -1,15 +1,14 @@
 module.exports = {
   settings: {
-    react: { version: "detect" },
+    react: { version: 'detect' },
   },
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "react/self-closing-comp": "error",
-    "react/require-default-props": 0,
-    "react/jsx-filename-extension": [
+    'react/react-in-jsx-scope': 0,
+    'react/self-closing-comp': 'error',
+    'react/jsx-filename-extension': [
       1,
-      { extensions: [".js", ".jsx", ".tsx"] },
+      { extensions: ['.js', '.jsx', '.tsx'] },
     ],
-    "react/jsx-props-no-spreading": 0,
+    'react/jsx-props-no-spreading': 0,
   },
-};
+}
--- typescript-react.js
+++ typescript-react.js
@@ -1,8 +1,12 @@
 module.exports = {
-  extends: ["airbnb", "airbnb/hooks", "./typescript", "./react", "./eslintrc"],
+  extends: ['airbnb', 'airbnb/hooks', './typescript', './react', './eslintrc'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
   },
-};
+  rules: {
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+  },
+}
