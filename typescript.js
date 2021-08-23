1bf5e283b14a856901747dab1e53aedb086c0744
Michal Vanko <mklakakilli@gmail.com>
2021-08-23 17:33:37 +0200

    Update airbnb eslint configuration

--- typescript.js
+++ typescript.js
@@ -18,6 +18,7 @@ module.exports = {
     },
   },
   extends: [
+    'airbnb-base',
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
